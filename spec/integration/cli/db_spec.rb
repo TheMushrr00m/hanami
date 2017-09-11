e9f1bffb0faad134019052b9517415eebc4c2dcc
Alfonso Uceda Pompa <alfonso@bebanjo.com>
2017-09-11 21:45:21 +0200

    Implement rollback command

--- spec/integration/cli/db_spec.rb
+++ spec/integration/cli/db_spec.rb
@@ -3,13 +3,14 @@ RSpec.describe "hanami db", type: :cli do
     with_project do
       output = <<-OUT
 Commands:
-  hanami db apply                           # Migrate, dump the SQL schema, and delete the migrations (experimental)
-  hanami db console                         # Starts a database console
-  hanami db create                          # Create the database (only for development/test)
-  hanami db drop                            # Drop the database (only for development/test)
-  hanami db migrate [VERSION]               # Migrate the database
-  hanami db prepare                         # Drop, create, and migrate the database (only for development/test)
-  hanami db version                         # Print the current migrated version
+  hanami db apply                              # Migrate, dump the SQL schema, and delete the migrations (experimental)
+  hanami db console                            # Starts a database console
+  hanami db create                             # Create the database (only for development/test)
+  hanami db drop                               # Drop the database (only for development/test)
+  hanami db migrate [VERSION]                  # Migrate the database
+  hanami db prepare                            # Drop, create, and migrate the database (only for development/test)
+  hanami db rollback [VERSIONS]                # Rollback the database
+  hanami db version                            # Print the current migrated version
 OUT
 
       run_command "hanami db", output, exit_status: 1
